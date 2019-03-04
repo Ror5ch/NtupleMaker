f1fe5f0b7602a290d29717fde911a2b7d5d21cf2
Ror5ch <namppl@gmail.com>
2019-03-04 11:26:34 +0100

    Updated the configuration for 2016 legacy data

--- NtupleMaker/test/data_Run2016_cfg.py
+++ NtupleMaker/test/data_Run2016_cfg.py
@@ -14,11 +14,11 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 ## Source
 process.source = cms.Source("PoolSource",
-    # fileNames = cms.untracked.vstring('')
-    fileNames = cms.untracked.vstring('/store/data/Run2016B/SinglePhoton/MINIAOD/17Jul2018_ver2-v1/50000/E87394E4-DC8B-E811-B452-0242AC1C0502.root')
+    fileNames = cms.untracked.vstring('/store/data/Run2016G/DoubleEG/MINIAOD/17Jul2018-v1/90000/E646696D-9F95-E811-AC0F-002590FD0F36.root'),
+    lumisToProcess = cms.untracked.VLuminosityBlockRange("279766:1-279766:999999")
 )
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
 
 ## Geometry and Detector Conditions (needed for a few patTuple production steps)
 process.load("Configuration.Geometry.GeometryRecoDB_cff")
@@ -36,6 +36,10 @@ process.tree.egm_corrected = cms.untracked.bool(True)
 process.tree.run_on_mc = isMC
 process.tree.reducedEBRecHits = cms.InputTag("reducedEgamma", "reducedEBRecHits", "DQM")
 
+# proton reconstruction
+process.load("RecoCTPPS.ProtonReconstruction.year_2016.ctppsProtonReconstruction_cfi")
+
 process.p = cms.Path(
+    process.ctppsProtonReconstruction *
     process.tree
 )
